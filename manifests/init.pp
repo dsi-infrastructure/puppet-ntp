26597c112bdf3b6b208155a208cf1290b8a612eb
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2013-07-13 05:19:16 -1000

    Factorisation et simplification du modules.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,35 @@
-class ntp::install {
-    package{"ntp":
-        ensure => latest
-    }
-}
+# class ntp
+#
 
-class ntp::config {
-   file { "/etc/ntp.conf":
-                notify => service["ntp"],
-                ensure => file,
-                require => Package['ntp'],
-                owner => root,
-                group => root,
-                mode => 644,
-                source => "puppet:///modules/ntp/ntp.conf",
-        }
-}
+class ntp($server="disabled") {
+
+	package{"ntp":
+		ensure => latest,
+	}
+
+	if $server == "enable" {
+		$servers = ["0.debian.pool.ntp.org iburst",
+								"1.debian.pool.ntp.org iburst",
+								"2.debian.pool.ntp.org iburst",
+								"3.debian.pool.ntp.org iburst", ]
+	} else {
+		$servers = ["1.ntp.srv.gov.pf iburst",
+								"2.ntp.srv.gov.pf iburst", ]
+	}
+
+	file { "/etc/ntp.conf":
+		notify	=> service["ntp"],
+		ensure	=> file,
+		require	=> Package['ntp'],
+		owner		=> root,
+		group		=> root,
+		mode		=> 644,
+		content	=> template("ntp/ntp.erb");
+	}
  
-class ntp::service {
-    service{"ntp":
-        ensure  => running,
-        enable  => true,
-        require => Class["ntp::config"],
-    }
-}
- 
-class ntp {
-    include ntp::install, ntp::config, ntp::service
+	service {"ntp":
+		ensure  => running,
+		enable  => true,
+		require => File["/etc/ntp.conf"],
+	}
 }
