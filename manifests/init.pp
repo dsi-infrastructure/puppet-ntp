136a876fc8c19059c3692392ba7546b5fd7486f2
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2013-07-14 16:26:36 -1000

    Validation syntaxique.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,35 @@
 # class ntp
 #
 
-class ntp($server="disabled") {
+class ntp($server='disabled') {
 
-	package{"ntp":
-		ensure => latest,
-	}
+  package{'ntp':
+    ensure => latest,
+  }
 
-	if $server == "enable" {
-		$servers = ["0.debian.pool.ntp.org iburst",
-								"1.debian.pool.ntp.org iburst",
-								"2.debian.pool.ntp.org iburst",
-								"3.debian.pool.ntp.org iburst", ]
-	} else {
-		$servers = ["1.ntp.srv.gov.pf iburst",
-								"2.ntp.srv.gov.pf iburst", ]
-	}
+  if $server == 'enable' {
+    $servers = ['0.debian.pool.ntp.org iburst',
+                '1.debian.pool.ntp.org iburst',
+                '2.debian.pool.ntp.org iburst',
+                '3.debian.pool.ntp.org iburst', ]
+  } else {
+    $servers = ['1.ntp.srv.gov.pf iburst',
+                '2.ntp.srv.gov.pf iburst', ]
+  }
 
-	file { "/etc/ntp.conf":
-		notify	=> service["ntp"],
-		ensure	=> file,
-		require	=> Package['ntp'],
-		owner		=> root,
-		group		=> root,
-		mode		=> 644,
-		content	=> template("ntp/ntp.erb");
-	}
- 
-	service {"ntp":
-		ensure  => running,
-		enable  => true,
-		require => File["/etc/ntp.conf"],
-	}
+  file { '/etc/ntp.conf':
+    ensure  => file,
+    require => Package['ntp'],
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('ntp/ntp.erb'),
+    notify  => Service['ntp'],
+  }
+
+  service {'ntp':
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/ntp.conf'],
+  }
 }
